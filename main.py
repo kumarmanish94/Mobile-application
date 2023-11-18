fe96fcd6bf97d05aad895a6cbbb4322676d74a24
Manish kumar <116753641+kumarmanish94@users.noreply.github.com>
2023-11-18 17:34:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+class SpartanGrid(GridLayout):
+    def __init__(self,**kwargs):
+        super(SpartanGrid, self).__init__()
+        self.cols=2
+        self.add_widget(Label(text="Student Name:"))
+
+        self.s_name = TextInput()
+        self.add_widget(self.s_name)
+
+        self.add_widget(Label(text="Student Marks:"))
+        self.s_marks = TextInput()
+        self.add_widget(self.s_marks)
+
+        self.add_widget(Label(text="Student Age:"))
+        self.s_age = TextInput()
+        self.add_widget(self.s_age)
+
+        self.add_widget(Label(text="Student Course:"))
+        self.s_course = TextInput()
+        self.add_widget(self.s_course)
+
+        self.add_widget(Label(text="Student Gender:"))
+        self.s_gender = TextInput()
+        self.add_widget(self.s_gender)
+
+        self.press = Button(text="Click me")
+        self.press.bind(on_press=self.click_me)
+        self.add_widget(self.press)
+
+    def click_me(self,instance):
+         print("Name of student is "+self.s_name.text)
+         print("Marks of student are "+self.s_marks.text)
+         print("Gender of student is "+self.s_gender.text)
+         print("Course of student is "+self.s_course.text)
+         print("Age of student is "+self.s_age.text)
+
+class SpartanApp(App):
+    def build(self):
+        return SpartanGrid()
+
+
+if __name__ == "__main__":
+    SpartanApp().run()
